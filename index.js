8122e60c181b01a26f37988d1f5b62220481acfb
Joshua Palacios <palacios.joshua@gmail.com>
2017-10-23 16:06:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -68,6 +68,7 @@ function createRock(x) {
    * This function moves the rock. (2 pixels at a time
    * seems like a good pace.)
    */
+ }
   function moveRock() {
     while (!checkCollision(rock) && top > 0) {
       top += 2
@@ -87,7 +88,6 @@ function createRock(x) {
      * we should call endGame()
      */
 
-     }
     /**
      * Otherwise, if the rock hasn't reached the bottom of
      * the GAME, we want to move it again.
@@ -97,7 +97,7 @@ function createRock(x) {
      * But if the rock *has* reached the bottom of the GAME,
      * we should remove the rock from the DOM
      */
-  }
+
   window.requestAnimationFrame(moveRock)
   // We should kick of the animation of the rock around here
 
